85eaab570b75cb48712ede44d0af874e5f05ffbb
bogzbonny <bogzbonny@protonmail.com>
2024-12-29 12:01:06 -0800

    pre context refactor

--- yeehaw/src/context.rs
+++ yeehaw/src/context.rs
@@ -7,10 +7,10 @@ use {
 /// Context is a struct which contains information about the current context of a
 /// given element.
 /// The context of an element is passed to the element during key function calls
-/// where the element may need to know its size
+/// where the element may need to know information relavent to its rendering.
 ///
-/// Additionally, metadata may be addended to the context to pass additional
-/// arbitrary information.
+/// Additionally, metadata may be added to a context to pass additional
+/// arbitrary information between elements.
 #[derive(Clone, Debug)]
 pub struct Context {
     /// The size of the element, during initialization of an element this size will be unknown and
